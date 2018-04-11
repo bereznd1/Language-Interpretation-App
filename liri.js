21faf1ac0c10821695a13e00b7851088cff70e5a
Dennis <bereznd1@tcnj.edu>
2018-04-11 13:26:06 -0400

    final code

--- liri.js
+++ liri.js
@@ -17,12 +17,15 @@ var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
 
 
-//If the user types in "my-tweets"...
+//Code for retrieving last 20 tweets
 if (command === "my-tweets") {
 
-
+    //Setting the parameters to equal the Twitter username we are retrieving tweets from
     var params = { screen_name: 'LiriDennisB' };
+
+    //Send a request to the Twitter API
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        
         if (error) {
             return console.log('Error occurred: ' + error);
         }
@@ -33,90 +36,75 @@ if (command === "my-tweets") {
 
         console.log("\n");
 
+        //If the specified Twitter username has less than 20 tweets, retrieve all of its tweets
         if (tweets.length < 20) {
             for (i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].text);
-                console.log("\n");
-                console.log("Created: " + tweets[i].created_at);
-                console.log("\n");
-                console.log("\n");
+                console.log("\nCreated: " + tweets[i].created_at + "\n\n");
             }
         }
 
+        //If the specified Twitter username has more than 20 tweets, retrieve the latest 20
         else {
-            for (i = 0; i < 19; i++) {
-
-
+            for (i = 0; i < 20; i++) {
                 console.log(tweets[i].text);
-                console.log("\n");
-                console.log("Created: " + tweets[i].created_at);
-                console.log("\n");
-                console.log("\n");
+                console.log("\nCreated: " + tweets[i].created_at + "\n\n");
             }
-
         }
-
-
     });
-
-
 }
 
 
-
+//Code for retrieving info about a certain song
 if (command === "spotify-this-song") {
 
-
+    //If the user has entered a song title as input...
     if (fullTitle) {
 
+        //Send a request to the Spotify API, using the song title the user has inputted as part of the query url
         spotify
             .request('https://api.spotify.com/v1/search?q=track:' + fullTitle + '&type=track')
             .then(function (data) {
-                console.log("Song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+                console.log("\nSong: " + JSON.stringify(data.tracks.items[0].name, null, 2));
                 console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
                 console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
                 console.log("Spotify Link: " + JSON.stringify(data.tracks.items[0].artists[0].external_urls.spotify, null, 2));
-
-
             })
+
             .catch(function (err) {
                 console.error('Error occurred: ' + err);
             });
-
     }
 
-
+    //If the user has NOT entered a song title as input...
     else {
+
+        //Send a request to the Spotify API, using the default track "The Sign" by Ace of Base as part of the query url
         spotify
             .request('https://api.spotify.com/v1/search?q=track:the+sign&type=track')
             .then(function (data) {
-                console.log("Song: " + JSON.stringify(data.tracks.items[12].name, null, 2));
+                console.log("\nSong: " + JSON.stringify(data.tracks.items[12].name, null, 2));
                 console.log("Artist: " + JSON.stringify(data.tracks.items[12].album.artists[0].name, null, 2));
                 console.log("Album: " + JSON.stringify(data.tracks.items[12].album.name, null, 2));
                 console.log("Spotify Link: " + JSON.stringify(data.tracks.items[12].artists[0].external_urls.spotify, null, 2));
-
-
-
-
             })
+
             .catch(function (err) {
                 console.error('Error occurred: ' + err);
             });
-
     }
-
 }
 
 
+//Code for retrieving info about a certain movie
 if (command === "movie-this") {
 
-
+    //If the user has entered a movie title as input...
     if (fullTitle) {
 
-        //Runs a request to the OMDB API with the movie title that the user typed in
+        //Sets up the queryUrl that will be sent to the OMDB API, using the movie title that the user typed in
         var queryUrl = "http://www.omdbapi.com/?t=" + fullTitle + "&y=&plot=short&apikey=trilogy";
 
-
         //Creates a request to the queryUrl
         request(queryUrl, function (error, response, body) {
 
@@ -124,35 +112,30 @@ if (command === "movie-this") {
                 return console.log('Error occurred: ' + error);
             }
 
-            // If the request is successful...
+            //If the request is successful...
             if (!error && response.statusCode === 200) {
 
-                console.log("\n");
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Year: " + JSON.parse(body).Year);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Country of Production: " + JSON.parse(body).Country);
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("\n\nTitle: " + JSON.parse(body).Title);
+                console.log("\nYear: " + JSON.parse(body).Year);
+                console.log("\nActors: " + JSON.parse(body).Actors);
+                console.log("\nPlot: " + JSON.parse(body).Plot);
+                console.log("\nLanguage: " + JSON.parse(body).Language);
+                console.log("\nCountry of Production: " + JSON.parse(body).Country);
+                console.log("\nIMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
 
             }
-
         });
-
     }
 
-
+    //If the user has NOT entered a movie title as input...
     else {
 
-        //Runs a request to the OMDB API with the movie title that the user typed in
+        //Sets up the queryUrl that will be sent to the OMDB API, using the default movie title "Mr. Nobody"
         var queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
-
 
         //Creates a request to the queryUrl
         request(queryUrl, function (error, response, body) {
-
 
             if (error) {
                 return console.log('Error occurred: ' + error);
@@ -161,44 +144,39 @@ if (command === "movie-this") {
             // If the request is successful...
             if (!error && response.statusCode === 200) {
 
-                console.log("\n");
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Year: " + JSON.parse(body).Year);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Country of Production: " + JSON.parse(body).Country);
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-
+                console.log("\n\nTitle: " + JSON.parse(body).Title);
+                console.log("\nYear: " + JSON.parse(body).Year);
+                console.log("\nActors: " + JSON.parse(body).Actors);
+                console.log("\nPlot: " + JSON.parse(body).Plot);
+                console.log("\nLanguage: " + JSON.parse(body).Language);
+                console.log("\nCountry of Production: " + JSON.parse(body).Country);
+                console.log("\nIMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
 
             }
-
         });
-
     }
-
-
 }
 
 
-
+//Code for calling the Spotify-This-Song command using text from the "Random.txt" local file
 if (command === "do-what-it-says") {
 
-
+    //Use the file system package to read the text in the "Random.txt" local file
     fs.readFile("random.txt", "utf-8", function (err, data) {
 
         if (err) {
             return console.log(err);
         }
 
-
+        //Create an array from the text in the "Random.txt" local file
         var dataArr = data.split(",");
 
+        //Send a request to the Spotify API, using the song title retrieved from the text in the "Random.txt" local file
         spotify
             .request('https://api.spotify.com/v1/search?q=track:' + dataArr[1] + '&type=track')
             .then(function (data) {
-                console.log("Song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+                console.log("\nSong: " + JSON.stringify(data.tracks.items[0].name, null, 2));
                 console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
                 console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
                 console.log("Spotify Link: " + JSON.stringify(data.tracks.items[0].artists[0].external_urls.spotify, null, 2));
@@ -208,33 +186,5 @@ if (command === "do-what-it-says") {
             .catch(function (err) {
                 console.error('Error occurred: ' + err);
             });
-
-
-
-
-
-
     });
-
-
-
-    // if (fullTitle) {
-
-    //     spotify
-    //         .request('https://api.spotify.com/v1/search?q=track:' + fullTitle + '&type=track')
-    //         .then(function (data) {
-    //             console.log("Song: " + JSON.stringify(data.tracks.items[0].name, null, 2));
-    //             console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
-    //             console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
-    //             console.log("Spotify Link: " + JSON.stringify(data.tracks.items[0].artists[0].external_urls.spotify, null, 2));
-
-
-    //         })
-    //         .catch(function (err) {
-    //             console.error('Error occurred: ' + err);
-    //         });
-
-    // }
-
-
 }
